b59db52d4b925672afb6d1ef9d49e890e415fa01
Stanislav Manilov <stanislav.manilov@gmail.com>
2012-11-28 22:00:37 +0000

    Implementing first instructions.

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,10 +1,6 @@
 if [[ $1 ]]; then
     PATH=$PATH:~/Work/University/Objective-Droid/build/Debug+Asserts/bin/
-    llc --march=dalvik $1 -o /home/szm/Work/University/Objective-Droid/surgical/java/BoilerPlate/bin/decoded/smali/edu/objdroid/boilerplate/MainActivity.smali
-    cd /home/szm/Work/University/Objective-Droid/surgical/java/BoilerPlate/bin
-    apktool b decoded/ BoilerPlate1.apk
-    echo "android" | jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore BoilerPlate1.apk androiddebugkey
-    adb install -r BoilerPlate1.apk
+    llc --march=dalvik $1 -o MainActivity.smali
 elif [[ condition ]]; then
     echo "Please provide a program to compile."
 fi
